c736769e5d97e33585c7f1dd1efac2b52f627373
unknown <shabnamzaresh@gmail.com>
2022-11-08 11:25:14 +0330

    delete and append

--- 02-data-structurs/01-linkedlist/LinkedList.py
+++ 02-data-structurs/01-linkedlist/LinkedList.py
@@ -7,15 +7,23 @@ class Node:
 class LinkedList:
     def __init__(self, nodes):
         self.head = None
+        self.rear = self.head
         self.length = 0
         if nodes is not None:
             self.head = Node(nodes[0])
             current_node = self.head
+            self.length += 1
             for item in nodes[1:]:
                 new_node = Node(item)
                 current_node.next = new_node
                 current_node = current_node.next
+                self.length += 1
+            self.rear = current_node
             current_node.next = None
+
+    @property
+    def get_length(self):
+        return self.length
 
     def traverse(self):
         current_node = self.head
@@ -23,10 +31,39 @@ class LinkedList:
             yield current_node.data
             current_node = current_node.next
 
+    def append(self, data):
+        current_node = Node(data)
+        self.rear.next = current_node
+        self.rear = self.rear.next
+        self.rear.next = None
+        self.length += 1
+
+    def delete(self, data):
+        current_node = self.head
+        prev_node = None
+        while current_node:
+            if current_node.data == data:
+                if current_node == self.head:
+                    self.head = self.head.next
+                    return
+                if current_node == self.rear:
+                    prev_node.next = None
+                    return
+                prev_node.next = current_node.next
+                current_node.next = None
+                self.length -= 0
+                return
+            prev_node = current_node
+            current_node = current_node.next
+
 
 def main():
-    nodes = ['a', 'b', 'c', 'd']
+    nodes = ['a']
     linkedlist = LinkedList(nodes)
+    for item in linkedlist.traverse():
+        print(item)
+    print('----------------------------')
+    linkedlist.delete('a')
     for item in linkedlist.traverse():
         print(item)
 
