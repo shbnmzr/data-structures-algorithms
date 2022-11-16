5ec603d69dbb0a965fcc9d7d664dc12b5cfbadc1
unknown <shabnamzaresh@gmail.com>
2022-11-16 12:33:18 +0330

    max subarray

--- problems/data-structures/Day01.py
+++ problems/data-structures/Day01.py
@@ -7,5 +7,27 @@ def contains_duplicate(nums: list) -> bool:
     return len(nums) > len(nums_set)
 
 
+# Given an integer array nums,
+# find the subarray which has the largest sum and return its sum.
 
+def maximum_subarray(nums: list) -> int:
+    newNum = maxTotal = nums[0]
+
+    for i in range(1, len(nums)):
+        newNum = max(nums[i], nums[i] + newNum)
+        maxTotal = max(newNum, maxTotal)
+
+    return maxTotal
+
+
+def main():
+    arr = [-2, +1, -1, 5, 7]
+    n = len(arr)
+
+    max_sum = maximum_subarray(arr)
+    print("Maximum contiguous sum is ", max_sum)
+
+
+if __name__ == '__main__':
+    main()
 
